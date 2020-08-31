b413bc8369440d94944f554dec4cf7375272b4e9
mli42 <mli@e3r6p6.42.fr>
2020-08-31 11:35:40 +0200

    FragTrap++

---
+++ cpp03/ex01/FragTrap.hpp
@@ -0,0 +1,39 @@
+#ifndef FRAGTRAP_CLASS_HPP
+# define FRAGTRAP_CLASS_HPP
+
+# include <iostream>
+# include <string>
+
+class FragTrap {
+	public:
+		FragTrap(void);
+		FragTrap(std::string name);
+		FragTrap(FragTrap const &src);
+		~FragTrap(void);
+
+		FragTrap	&operator=(FragTrap const &rhs);
+
+		unsigned int	HitPoints;
+		unsigned int	MaxHitPoints;
+		unsigned int	energy;
+		unsigned int	MaxEnergy;
+		unsigned int	level;
+		std::string		name;
+		unsigned int	MeleeDamage;
+		unsigned int	RangedDamage;
+		unsigned int	ArmorDamageReduction;
+
+		void		rangedAttack(std::string const &target) const;
+		void		meleeAttack(std::string const &target) const;
+		void		takeDamage(unsigned int amount);
+		void		beRepaired(unsigned int amount);
+
+		void		vaulthunter_dot_exe(std::string const &target);
+
+	private:
+		void		SayQuote(std::string quote) const;
+};
+
+std::ostream	&operator<<(std::ostream &o, FragTrap const & i);
+
+#endif // ******************************************** FRAGTRAP_CLASS_HPP end //
--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -6,28 +6,47 @@
 /*   By: mli <mli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/30 18:46:11 by mli               #+#    #+#             */
-/*   Updated: 2020/08/31 10:41:24 by mli              ###   ########.fr       */
+/*   Updated: 2020/08/31 11:22:18 by mli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "FragTrap.hpp"
 #include "ScavTrap.hpp"
 
 int	main(void)
 {
-	ScavTrap	robot("RobotName");
+	FragTrap	FragBot("FragBotName");
+	ScavTrap	ScavBot("ScavBotName");
 
-	robot.meleeAttack("some target");
-	robot.rangedAttack("some target");
+	FragBot.meleeAttack("some target");
+	FragBot.rangedAttack("some target");
 
-	robot.takeDamage(60);
-	robot.takeDamage(60);
+	FragBot.takeDamage(60);
+	FragBot.takeDamage(60);
 
-	robot.beRepaired(20);
-	robot.beRepaired(200);
+	FragBot.beRepaired(20);
+	FragBot.beRepaired(200);
 
-	robot.challengeNewcomer("a rock1");
-	robot.challengeNewcomer("a rock2");
-	robot.challengeNewcomer("a rock3");
-	robot.challengeNewcomer("a rock4");
-	robot.challengeNewcomer("a rock5");
+	FragBot.vaulthunter_dot_exe("a rock1");
+	FragBot.vaulthunter_dot_exe("a rock2");
+	FragBot.vaulthunter_dot_exe("a rock3");
+	FragBot.vaulthunter_dot_exe("a rock4");
+	FragBot.vaulthunter_dot_exe("a rock5");
+
+// #######################################################
+
+	ScavBot.meleeAttack("some target");
+	ScavBot.rangedAttack("some target");
+
+	ScavBot.takeDamage(60);
+	ScavBot.takeDamage(60);
+
+	ScavBot.beRepaired(20);
+	ScavBot.beRepaired(200);
+
+	ScavBot.challengeNewcomer("a rock1");
+	ScavBot.challengeNewcomer("a rock2");
+	ScavBot.challengeNewcomer("a rock3");
+	ScavBot.challengeNewcomer("a rock4");
+	ScavBot.challengeNewcomer("a rock5");
 }
