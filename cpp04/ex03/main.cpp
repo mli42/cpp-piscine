be3f3783fecba70ad9355e2adb16ed6113532f36
mli42 <mli@student.42.fr>
2020-09-07 14:19:53 +0200

    cpp04/ex03 seem to be ok

--- cpp04/ex03/main.cpp
+++ cpp04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mli <mli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/07 11:26:44 by mli               #+#    #+#             */
-/*   Updated: 2020/09/07 12:30:54 by mli              ###   ########.fr       */
+/*   Updated: 2020/09/07 14:13:36 by mli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int		main(void)
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
 
-	ICharacter* me = new Character("me");
+	Character* me = new Character("me");
 
 	AMateria* tmp;
 	tmp = src->createMateria("ice");
@@ -56,13 +56,10 @@ int		main(void)
 	me->equip(tmp);
 	tmp = src->createMateria("ice");
 	me->equip(tmp);
-	tmp = src->createMateria("cure");
-	me->equip(tmp);
+	me->equip(src->createMateria("cure"));
 
 	bob = new Character("TargetBob");
 
-	me->unequip(1);
-	me->unequip(0);
 	me->use(-1, *bob);
 	me->use(0, *bob);
 	me->use(1, *bob);
@@ -72,17 +69,28 @@ int		main(void)
 
 	std::cout << "#################################" << std::endl;
 
-	Character mecpy = Character(*me);
-	mecpy.use(-1, *bob);
-	mecpy.use(0, *bob);
-	mecpy.use(1, *bob);
-	mecpy.use(2, *bob);
-	mecpy.use(1, *bob);
-	mecpy.use(3, *bob);
+	Character *mecpy = me;
+	mecpy->use(-1, *bob);
+	mecpy->use(0, *bob);
+	mecpy->use(1, *bob);
+	mecpy->use(2, *bob);
+	mecpy->use(3, *bob);
+	mecpy->use(4, *bob);
 
-	delete bob;
-	delete me;
-	delete src;
+	std::cout << "#################################" << std::endl;
+	for (int i = -1; i < 5; i++)
+		me->unequip(i);
+	for (int i = -1; i < 5; i++)
+		me->use(i, *bob);
 
+	std::cout << "#################################" << std::endl;
+	std::cout << "Last MateriaSource XP: " << tmp->getXP() << std::endl;
+	delete bob; bob = NULL;
+	delete me; me = NULL;
+	delete src; src = NULL;
+	tmp = NULL;
+	std::cout << "#################################" << std::endl;
+
+//	system("leaks a.out");
 	return (0);
 }
