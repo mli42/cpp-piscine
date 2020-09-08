65f75742daafae6dd625a5b2dec2d83feb182cab
mli42 <mli@student.42.fr>
2020-09-08 11:03:23 +0200

    cpp05/ex00

---
+++ cpp05/ex00/Bureaucrat.cpp
@@ -0,0 +1,51 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(void) {
+	return ;
+}
+
+Bureaucrat::Bureaucrat(std::string const name, unsigned short int const grade) : \
+		_name(name), _grade(grade) {
+	return ;
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat const &src) {
+	*this = src;
+}
+
+Bureaucrat::~Bureaucrat(void) {
+	return ;
+}
+
+Bureaucrat	&Bureaucrat::operator=(Bureaucrat const &rhs) {
+	if (this == &rhs)
+		return (*this);
+	this->_grade = rhs._grade;
+	return (*this);
+}
+
+std::ostream	&operator<<(std::ostream &o, Bureaucrat const &i) {
+	o << i.getName() << ", bureaucrat grade " << i.getGrade() << ".";
+	return (o);
+}
+
+const char	*Bureaucrat::GradeTooLowException::what(void) const throw() {
+	return ("Bureaucrat Grade Too Low");
+}
+
+const char	*Bureaucrat::GradeTooHighException::what(void) const throw() {
+	return ("Bureaucrat Grade Too High");
+}
+
+std::string const	Bureaucrat::getName(void) const { return (this->_name); }
+int					Bureaucrat::getGrade(void) const { return (this->_grade); }
+
+void				Bureaucrat::incGrade(void) {
+	if (this->_grade-- == 1)
+		throw Bureaucrat::GradeTooHighException();
+}
+
+void				Bureaucrat::decGrade(void) {
+	if (this->_grade++ == 150)
+		throw Bureaucrat::GradeTooLowException();
+}
---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,38 @@
+#ifndef BUREAUCRAT_CLASS_HPP
+# define BUREAUCRAT_CLASS_HPP
+
+# include <iostream>
+# include <string>
+# include <exception>
+
+class Bureaucrat {
+	protected:
+		Bureaucrat(void);
+	public:
+		Bureaucrat(std::string const name, unsigned short int const grade);
+		Bureaucrat(Bureaucrat const &src);
+		virtual ~Bureaucrat(void);
+
+		Bureaucrat	&operator=(Bureaucrat const &rhs);
+
+		void			incGrade(void);
+		void			decGrade(void);
+
+		class GradeTooLowException : public std::exception {
+			virtual const char *what() const throw();
+		};
+		class GradeTooHighException : public std::exception {
+			virtual const char *what() const throw();
+		};
+
+		std::string const	getName(void) const;
+		int					getGrade(void) const;
+	protected:
+		const std::string	_name;
+		unsigned short int	_grade;
+	private:
+};
+
+std::ostream	&operator<<(std::ostream &o, Bureaucrat const & i);
+
+#endif // ****************************************** BUREAUCRAT_CLASS_HPP end //
