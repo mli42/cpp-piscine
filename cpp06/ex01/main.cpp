ea5d38369e2e5465d8b746c7c13cfd7d2cb16d6d
mli42 <mli@student.42.fr>
2020-09-10 12:06:36 +0200

    serialization

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mli <mli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/09 16:47:18 by mli               #+#    #+#             */
-/*   Updated: 2020/09/09 17:03:47 by mli              ###   ########.fr       */
+/*   Updated: 2020/09/10 10:55:33 by mli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,24 +20,58 @@ typedef struct Data
 	std::string	s2;
 }				t_data;
 
-// const int alea = std::rand() % sizeof(name) / sizeof(*name);
-
 void	*serialize(void)
 {
+    static const char alphanum[] = \
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ" \
+        "abcdefghijklmnopqrstuvwxyz" \
+		"0123456789";
 	char	*data = new char[sizeof(char) * (8 * 2) + sizeof(int) * 1];
-	int		i = 0;
+	unsigned short int	i = 0;
+	static unsigned short int dorand = 0;
 
+	if (dorand == 0)
+	{
+		std::srand(std::time(NULL));
+		dorand++;
+	}
+
+	for (int j = 0; j < 8; j++)
+		data[i++] = alphanum[std::rand() % (sizeof(alphanum) - 1)];
+	for (int j = 0; j < 4; j++)
+		data[i++] = alphanum[std::rand() % 255];
+	for (int j = 0; j < 8; j++)
+		data[i++] = alphanum[std::rand() % (sizeof(alphanum) - 1)];
 	return (data);
+}
+
+Data	*deserialize(void *raw)
+{
+	Data	*deserialized = new Data;
+
+	deserialized->s1 = std::string(static_cast<char *>(raw), 8);
+	deserialized->n = *reinterpret_cast<int *>(static_cast<char *>(raw) + 8);
+	deserialized->s2 = std::string(static_cast<char *>(raw) + 8 + 4, 8);
+	return (deserialized);
+}
+
+void	DisplayData(Data *data)
+{
+	std::cout << "data->s1: [" << data->s1 << "]" << std::endl;
+	std::cout << "data->n: [" << data->n << "]" << std::endl;
+	std::cout << "data->s2: [" << data->s2 << "]" << std::endl;
 }
 
 int		main(void)
 {
-	void	*data;
+	void	*raw;
+	Data	*data;
 
-	std::srand(std::time(NULL));
+	raw = serialize();
+	data = deserialize(raw);
 
-	data = serialize();
-
-	delete [] reinterpret_cast<char *>(data);
+	DisplayData(data);
+	delete [] static_cast<char *>(raw);
+	delete data;
 	return (0);
 }
