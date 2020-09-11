9623f697b863417f762141e756fa7b40434b12e6
mli42 <mli@student.42.fr>
2020-09-11 10:15:58 +0200

    cpp07/ex00 better main

--- cpp07/ex00/whatever.cpp
+++ cpp07/ex00/whatever.cpp
@@ -6,7 +6,7 @@
 /*   By: mli <mli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/10 16:24:46 by mli               #+#    #+#             */
-/*   Updated: 2020/09/10 17:01:33 by mli              ###   ########.fr       */
+/*   Updated: 2020/09/11 10:14:23 by mli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,21 @@ template<typename T>
 T const	&max(T const &a, T const &b) { return (a > b ? a : b); }
 
 template<typename T>
+std::string const	DispVar(std::ostream &o, T const &var)
+{
+	o << "\e[93m" << var << "\e[m";
+	return ("");
+}
+
+template<typename T>
 void	DispSwap(T &a, T &b)
 {
 	std::cout << "###############################################" << std::endl;
-	std::cout << "Swaps	a:" << a << "\tb:" << b << std::endl; 
+	std::cout << "Swaps	a:" << DispVar(std::cout, a) \
+		<< "\tb:" << DispVar(std::cout, b) << std::endl; 
 	swap(a, b);
-	std::cout << "Swaped	a:" << a << "\tb:" << b << std::endl; 
+	std::cout << "Swaped	a:" << DispVar(std::cout, a) \
+		<< "\tb:" << DispVar(std::cout, b) << std::endl; 
 	std::cout << "###############################################" << std::endl;
 }
 
@@ -44,8 +53,9 @@ void	DispCmp(T const &a, T const &b, T const &(*fct)(T const &, T const &), \
 {
 	T const &tmp = fct(a, b);
 	std::cout << "###############################################" << std::endl;
-	std::cout << fct_type << " with a:" << a << " b:" << b << " ==> " \
-		<< tmp << std::endl;
+	std::cout << fct_type << " with a:" << DispVar(std::cout, a) \
+		<< " b:" << DispVar(std::cout, b) \
+		<< " ==> " << DispVar(std::cout, tmp)  << std::endl;
 	std::cout << "Addresses are..." << std::endl;
 	std::cout << "- a:\t" << &a << std::endl;
 	std::cout << "- b:\t" << &b << std::endl;
@@ -53,20 +63,28 @@ void	DispCmp(T const &a, T const &b, T const &(*fct)(T const &, T const &), \
 	std::cout << "###############################################" << std::endl;
 }
 
+#define MAXSTR 0
+#define MINSTR 1
+
 int		main(void)
 {
 	int a = 24; int b = 42;
 	int equal_ONE = 30; int equal_TWO = 30;
 	std::string str[2] = {"First string", "Second string"};
+	const std::string mstr[2] = {"\e[91mmax\e[m", "\e[92mmin\e[m"};
 
 	DispSwap(a, b);
 	DispSwap(str[0], str[1]);
 	DispSwap<std::string>(str[0], str[1]);
 
-	DispCmp(a, b, min, "min");
-	DispCmp(a, b, max, "max");
+	DispCmp(a, b, max, mstr[MAXSTR]);
+	DispCmp(a, b, min, mstr[MINSTR]);
 
-	DispCmp(equal_ONE, equal_TWO, max, "max");
-	DispCmp(equal_ONE, equal_TWO, min, "min");
+	float fa = 24.24; float fb = 42.42;
+	DispCmp(fa, fb, max, mstr[MAXSTR]);
+	DispCmp(fa, fb, min, mstr[MINSTR]);
+
+	DispCmp(equal_ONE, equal_TWO, max, mstr[MAXSTR]);
+	DispCmp(equal_ONE, equal_TWO, min, mstr[MINSTR]);
 	return (0);
 }
