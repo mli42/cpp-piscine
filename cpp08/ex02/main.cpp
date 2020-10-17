7f07803cd5abc113eaf99dde2f4a877e94ddffd2
mli <mli@student.42.fr>
2020-10-17 16:19:43 +0200

    cpp08 better main

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -6,14 +6,50 @@
 /*   By: mli <mli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/13 23:35:40 by mli               #+#    #+#             */
-/*   Updated: 2020/09/13 23:35:41 by mli              ###   ########.fr       */
+/*   Updated: 2020/10/17 16:19:02 by mli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mutantstack.hpp"
+#include <list>
+
+void	lst_main(void)
+{
+	std::cout << "--------------- std::list ---------------------" << std::endl;
+	std::list<int> mstack;
+
+	mstack.push_back(5);
+	mstack.push_back(17);
+
+	std::cout << mstack.back() << std::endl;
+
+	mstack.pop_back();
+
+	std::cout << mstack.size() << std::endl;
+
+	mstack.push_back(3);
+	mstack.push_back(5);
+	mstack.push_back(737);
+	//[...]
+	mstack.push_back(0);
+
+	std::list<int>::iterator it = mstack.begin();
+	std::list<int>::iterator ite = mstack.end();
+
+	++it;
+	--it;
+
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::cout << "-----------------------------------------------" << std::endl;
+}
 
 int		main(void)
 {
+	std::cout << "MutantStack" << std::endl;
 	MutantStack<int> mstack;
 
 	mstack.push(5);
@@ -44,5 +80,12 @@ int		main(void)
 	}
 	std::stack<int> s(mstack);
 	std::cout << "-----------------------------------------------" << std::endl;
+
+	for (it = mstack.begin(); it != ite; ++it)
+		*it = 0;
+	std::cout << "New content of mutantstack" << std::endl;
+	for (it = mstack.begin(); it != ite; ++it)
+		std::cout << *it << std::endl;
+	lst_main();
 	return (0);
 }
